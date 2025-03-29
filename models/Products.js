b011396e5f8d8858b783ef7e7de3af9bf824cc24
Sajithjay99 <sajithjay99@gmail.com>
2025-03-30 00:42:53 +0530

    feat: add brand and dateAdded fields to product model

--- models/Products.js
+++ models/Products.js
@@ -1,60 +1,63 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Product name is required'],
-        trim: true,
-      },
-    
-      category: {
-        type: String,
-        required: [true, 'Category is required'],
-        enum: [
-          'Adult Care',
-          'Beauty Accessories',
-          'Beverages',
-          'Cosmetics',
-          'Dairy Products',
-          'Kids',
-          'Mother & Baby Care',
-          'Personal Care',
-          'Pet Care',
-          'Skin Care',
-          'Surgical Items',
-          'Vitamins & Nutritions',
-          'Others',
-        ],
-      },
-    
-      description: {
-        type: String,
-        required: false,
-      },
-    
-      price: {
-        type: Number,
-        required: [true, 'Price is required'],
-        min: 0,
-      },
-    
-      availability: {
-        type: Boolean,
-        default: true,
-      },
-    
-      expiryDate: {
-        type: Date,
-        required: false,
-      },
-    
-      images: [
-         String
-       
-      ],
-    
-    });
-    
+  name: {
+    type: String,
+    required: [true, 'Product name is required'],
+    trim: true,
+  },
+
+  category: {
+    type: String,
+    required: [true, 'Category is required'],
+    enum: [
+      'Adult Care',
+      'Beauty Accessories',
+      'Beverages',
+      'Cosmetics',
+      'Dairy Products',
+      'Kids',
+      'Mother & Baby Care',
+      'Personal Care',
+      'Pet Care',
+      'Skin Care',
+      'Surgical Items',
+      'Vitamins & Nutritions',
+      'Others',
+    ],
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+    min: 0,
+  },
+
+  brand: {
+    type: String,
+    required: [true, 'Brand is required'],
+    trim: true,
+  },
+
+  description: {
+    type: String,
+    required: false,
+  },
+
+  availability: {
+    type: Boolean,
+    default: true,
+  },
+
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+
+  images: [
+    String
+  ],
+});
 
 const Product = mongoose.model('Product', productSchema);
 export default Product;
